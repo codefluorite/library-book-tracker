dd2b020545ade12ee4380e953df9406fd8296689
codefluorite <83612917+codefluorite@users.noreply.github.com>
2025-03-14 20:50:29 +0000

    Basic setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Layout from "./components/layout";
+import Homepage from "./pages/homepage";
+import Addbook from "./pages/add-book";
+import Updatebook from "./pages/update-book";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/add-book" element={<Addbook />} />
+            <Route path="/update-book" element={<Updatebook />} />
+          </Routes>
+        </Layout>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
